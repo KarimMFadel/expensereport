2573e7f332dd6044659473d7b0c184c8ac16ac55
Karim Fadel <karimmm.fadel@gmail.com>
2025-12-31 00:04:50 +0200

    add InvoiceItems list with InvoiceData

--- expensereport-java/src/main/java/com/nelkinda/training/ExpenseReport.java
+++ expensereport-java/src/main/java/com/nelkinda/training/ExpenseReport.java
@@ -17,11 +17,13 @@ class InvoiceData {
     Date date;
     int totalExpenses;
     int totalMealExpenses;
+    List<InvoiceItem> invoiceItems;
 
-    public InvoiceData(Date date, int totalExpenses, int totalMealExpenses) {
+    public InvoiceData(Date date, int totalExpenses, int totalMealExpenses, List<InvoiceItem> invoiceItems) {
         this.date = date;
         this.totalExpenses = totalExpenses;
         this.totalMealExpenses = totalMealExpenses;
+        this.invoiceItems = invoiceItems;
     }
 }
 
@@ -39,21 +41,26 @@ class InvoiceItem {
 
 public class ExpenseReport {
     public void printReport(List<Expense> expenses) {
-        InvoiceData invoiceData = new InvoiceData(new Date(), getTotalExpenses(expenses), getTotalMealExpenses(expenses));
+        List<InvoiceItem> invoiceItems = calculateInvoiceItems(expenses);
+        InvoiceData invoiceData = new InvoiceData(new Date(), getTotalExpenses(expenses), getTotalMealExpenses(expenses), invoiceItems);
 
         System.out.println("Expenses " + invoiceData.date);
 
-        List<InvoiceItem> invoiceItems = new ArrayList<>();
-        for (Expense expense : expenses) {
-            InvoiceItem expenseItem = new InvoiceItem(getExpenseName(expense), expense.amount, checkMealOverExpenses(expense));
-            invoiceItems.add(expenseItem);
-        }
         for (InvoiceItem expenseItem : invoiceItems) {
             System.out.println(expenseItem.expenseName + "\t" + expenseItem.amount + "\t" + expenseItem.mealOverExpensesMarker);
         }
 
         System.out.println("Meal expenses: " + invoiceData.totalMealExpenses);
         System.out.println("Total expenses: " + invoiceData.totalExpenses);
+    }
+
+    private static List<InvoiceItem> calculateInvoiceItems(List<Expense> expenses) {
+        List<InvoiceItem> invoiceItems = new ArrayList<>();
+        for (Expense expense : expenses) {
+            InvoiceItem expenseItem = new InvoiceItem(getExpenseName(expense), expense.amount, checkMealOverExpenses(expense));
+            invoiceItems.add(expenseItem);
+        }
+        return invoiceItems;
     }
 
     private static int getTotalExpenses(List<Expense> expenses) {
