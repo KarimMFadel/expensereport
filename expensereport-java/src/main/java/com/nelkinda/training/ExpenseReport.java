40fcb22603967c7afc08ed992c71f1c9fab5ac78
Karim Fadel <karimmm.fadel@gmail.com>
2025-12-30 22:37:30 +0200

    iv

--- expensereport-java/src/main/java/com/nelkinda/training/ExpenseReport.java
+++ expensereport-java/src/main/java/com/nelkinda/training/ExpenseReport.java
@@ -38,8 +38,7 @@ public class ExpenseReport {
     }
 
     private static String checkMealOverExpenses(Expense expense) {
-        String mealOverExpensesMarker = expense.type == ExpenseType.DINNER && expense.amount > 5000 || expense.type == ExpenseType.BREAKFAST && expense.amount > 1000 ? "X" : " ";
-        return mealOverExpensesMarker;
+        return expense.type == ExpenseType.DINNER && expense.amount > 5000 || expense.type == ExpenseType.BREAKFAST && expense.amount > 1000 ? "X" : " ";
     }
 
     private static String getExpenseName(Expense expense) {
